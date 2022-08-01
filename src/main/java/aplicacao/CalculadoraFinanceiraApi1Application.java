ea0d91c903adb4e02b971a03dca98603910293f3
Leandro Soares <leoarcabold@gmail.com>
2022-08-01 12:12:33 -0300

    docs: primeiro commit

---
+++ src/main/java/aplicacao/CalculadoraFinanceiraApi1Application.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraFinanceiraApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraFinanceiraApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/aplicacao/CalculadoraFinanceiraApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraFinanceiraApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
