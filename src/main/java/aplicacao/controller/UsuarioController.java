365c8dbf72efc6a5f25ab4777a7b558e058b28dc
Leandro Soares <leoarcabold@gmail.com>
2022-08-02 12:39:04 -0300

    feat: add pom

---
+++ src/main/java/aplicacao/controller/UsuarioController.java
@@ -0,0 +1,11 @@
+package aplicacao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value="/usuario")
+public class UsuarioController {
+
+	
+}
